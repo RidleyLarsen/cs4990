32a87f82452f4df6b0087677ece55f78f2469a48
Ridley Larsen <ridley@velocitywebworks.com>
2015-12-02 18:31:35 -0700

    Updated fabfile

--- project9/fabfile.py
+++ project9/fabfile.py
@@ -9,6 +9,12 @@ def restart():
 
 
 @hosts('root@198.199.109.62')
+def staticfiles():
+    env.cwd = '/home/django/cs4990/project9'
+    run('/home/django/cs4990/venv-django/bin/python manage.py collectstatic -l')
+
+
+@hosts('root@198.199.109.62')
 def deploy():
     env.cwd = '/home/django/cs4990/'
     run('git pull')
