574a86dfb2afd6990933ba15fa431cd2320f0cb2
Ridley Larsen <ridley@velocitywebworks.com>
2015-12-02 18:22:05 -0700

    Updated fabfile and added restart command.

--- project9/fabfile.py
+++ project9/fabfile.py
@@ -1,6 +1,15 @@
 from fabric.api import hosts, run, env
 
+
+@hosts('root@198.199.109.62')
+def restart():
+    env.cwd = '/home/django/cs4990/project9/'
+    run('kill -9 $(cat /home/django/cs4990/project9/supervisord.pid)')
+    run('supervisord -c supervisord.conf')
+
+
 @hosts('root@198.199.109.62')
 def deploy():
     env.cwd = '/home/django/cs4990/'
     run('git pull')
+    restart()
